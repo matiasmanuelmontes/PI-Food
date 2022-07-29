860d17484883495f01d7f4419cc173e9305591c9
matiasmanuelmontes <matiasmanuelmontes@gmail.com>
2022-07-29 20:08:50 -0300

    actualizacion 29/07

---
+++ api/src/models/Diet.js
@@ -0,0 +1,19 @@
+const { DataTypes, Sequelize } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('genre', {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      //defaultValue: Sequelize.UUIDV4
+      defaultValue: DataTypes.UUIDV4  // esto me genera un UUIDV4 automatico por que el id autoincremental puede no estar
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  });
+};
