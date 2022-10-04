15120363c05a8b77751f7403f3627f966498bac0
matiasmanuelmontes <matiasmanuelmontes@gmail.com>
2022-10-04 18:43:09 -0300

    cambio las cosas para deployar el back

--- api/index.js
+++ api/index.js
@@ -33,7 +33,7 @@ conn.sync({ force: true }).then(() => {
 // Proyecto con deploy
 
 require("dotenv").config();
-const server = require("./src/App.js");
+const server = require("./src/app.js");
 const PORT = 3001;
 const { conn } = require("./src/db.js");
 const cors = require('cors');
