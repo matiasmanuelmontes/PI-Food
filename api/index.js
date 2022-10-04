433ee476019c93a3a7f4edcddaf3077febefe2a9
matiasmanuelmontes <matiasmanuelmontes@gmail.com>
2022-10-04 18:42:13 -0300

    cambio las cosas para deployar el back

--- api/index.js
+++ api/index.js
@@ -38,9 +38,9 @@ const PORT = 3001;
 const { conn } = require("./src/db.js");
 const cors = require('cors');
 
-/* conn.sync({ force: false }).then(() => {
+ conn.sync({ force: false }).then(() => {
   server.use(cors({ credentials: true }));
   server.listen(process.env.PORT || PORT, () => {
     console.log(`Servidor Activo!`);
   });
-}); */
+}); 
