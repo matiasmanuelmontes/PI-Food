0671cfcc3cf756858bc4d30056e504533487500b
matiasmanuelmontes <matiasmanuelmontes@gmail.com>
2023-01-03 19:49:53 -0300

    allow all en 1

--- api/src/DB_variables.js
+++ api/src/DB_variables.js
@@ -5,7 +5,7 @@ const DB_USER="postgres";
 const DB_PASSWORD="12345m4";
 const DB_HOST="localhost";
 /* const API_KEY="993943c6a7d4a29527f6e9c92b7d0541"; */
-const ALLOW_ALL = 0;
+const ALLOW_ALL = 1;
  
 
 
