7f39dc3de2f75d32512752dc7c92f5d2b4995f1a
matiasmanuelmontes <matiasmanuelmontes@gmail.com>
2023-01-11 20:39:56 -0300

    hago varios cambios de css

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -6,9 +6,11 @@ export default function LandingPage() {
     return (
         <div className="landingBackGround">
             <h1 className="landingTitlePincipal"> Welcome to the Recipes Finder Page </h1>
+            <p className="landingDescription">In this website you can find the recipe you want, press the get in button and redirect to our home page! </p>
             <Link to='/recipes/'>
                 <button className="landingHomeButton">Get In</button>
             </Link>
+            
         </div>
     )
 }
